89f30e7b10194cbd22b5df831b879229f17ec05a
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 03:52:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,7 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   array = [
-    {name:  "Don Gately", occupation: "Live-in Staff"},
+    { name:  "Don Gately", occupation: "Live-in Staff"},
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
@@ -20,12 +20,13 @@ end
 
 def literal_aoh
   array = [
-    {name:  "Don Gately", occupation: "Live-in Staff"},
+    { name:  "Don Gately", occupation: "Live-in Staff"},
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
+    
   array.sort
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
