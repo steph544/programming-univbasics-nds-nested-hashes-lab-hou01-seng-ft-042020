e033fc49c3d7ff96c8346e3b6a09f92f6d6c708a
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 03:55:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,8 +26,6 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
-    
-  array.sort
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
